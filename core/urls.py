6f0dc7b95fe34506b0ffd70893b2080d75660c7f
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-03-11 15:25:01 +0500

    ACCOUNTS: templates added

--- core/urls.py
+++ core/urls.py
@@ -44,17 +44,17 @@ urlpatterns = [
 
     path(
         'reset/password/sent/',
-        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/../templates/account/password_reset_sent.html"),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>',
-        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/../templates/account/password_reset_form.html"),
         name='password_reset_confirm'
     ),
     path(
         'reset/password/complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/../templates/account/password_reset_done.html"),
         name='password_reset_complete'
     ),
 
