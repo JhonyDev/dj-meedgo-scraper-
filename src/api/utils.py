eb8303c571bb0dfe1ed3075be647ef11c77ad98d
jhonydev <star.jk901@gmail.com>
2022-10-10 00:21:21 +0500

    changes made

--- src/api/utils.py
+++ src/api/utils.py
@@ -28,7 +28,8 @@ def days_between(d1, d2):
 def get_availability(date, end_date):
     from .models import Booking, Room, Category
     parent_dict = {"Total": 0}
-    end_date = end_date - datetime.timedelta(days=2)
+    date = date + datetime.timedelta(days=1)
+    print(end_date)
     category = Category.objects.all()
     for cat in category:
         parent_dict[cat.name] = 0
