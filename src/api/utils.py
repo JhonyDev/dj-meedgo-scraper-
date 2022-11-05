4c68db55b1483f0640997fb7fe7108b773dbc740
jhonydev <star.jk901@gmail.com>
2022-11-05 09:51:39 +0500

    asd

--- src/api/utils.py
+++ src/api/utils.py
@@ -82,7 +82,7 @@ def generate_pdf_get_path(url):
         # pdfkit.from_url(url, f'media/{id_}.pdf', configuration=config)
         pdfkit.from_url(url, f'media/{id_}.pdf')
     except Exception as e:
-        print(str(e))
+        pass
 
     return f'media/{id_}.pdf'
 
