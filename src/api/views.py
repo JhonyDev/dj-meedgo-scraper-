1396bd6aba6a18daed1c2e3d330e849cc62ae8ea
jhonydev <star.jk901@gmail.com>
2022-12-11 17:14:28 +0500

    changes made

--- src/api/views.py
+++ src/api/views.py
@@ -350,7 +350,8 @@ class BookingGetAPIView(APIView):
 
     def get(self, request, date_, format=None):
         date_ = parser.parse(date_, dayfirst=True)
-        bookings = Booking.objects.filter(check_in_date__lte=date_, check_out_date__gte=date_)
+
+        bookings = Booking.objects.filter(check_in_date__lte=date_, check_out_date__gt=date_)
         booking_array = []
 
         for booking in bookings:
