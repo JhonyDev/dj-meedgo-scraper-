eb9ca45ca7a02643398f9deff9a20b76f779050a
jhonydev <star.jk901@gmail.com>
2023-05-05 21:37:15 +0500

    changes made

--- src/api/views.py
+++ src/api/views.py
@@ -21,10 +21,10 @@ class MedicineToCartView(generics.GenericAPIView):
     serializer_class = MedicineToCartSerializer
     queryset = MedicineCart.objects.all()
 
-    def get(self, request):
-        return Response({'message': 'Please use POST Method. Provide "cart_id" to get Cart Data '
-                                    'or Provide "medicine_id" to create new cart and add medicine'},
-                        status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    # def get(self, request):
+    #     return Response({'message': 'Please use POST Method. Provide "cart_id" to get Cart Data '
+    #                                 'or Provide "medicine_id" to create new cart and add medicine'},
+    #                     status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def post(self, request):
         return add_medicine_to_card(self, request)
