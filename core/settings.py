e029f08eed1c74ceedbf1b91f96fc18c32a5d523
jhonydev <star.jk901@gmail.com>
2023-05-08 02:30:23 +0500

    changes made

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ else:
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
+    'http://localhost:3030', 'http://localhost:5173',
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
--- core/tests.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-print(name)
-print(salt_name)
-print(final_price)
-print(exists)
