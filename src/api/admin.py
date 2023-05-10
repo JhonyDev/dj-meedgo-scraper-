65d008b8e8665f5f66cd39381c9e88f78479d751
jhonydev <star.jk901@gmail.com>
2023-05-11 00:02:31 +0500

    changes made

--- src/api/admin.py
+++ src/api/admin.py
@@ -5,6 +5,8 @@ from . import models
 
 class MedicineView(admin.ModelAdmin):
     list_display = ['pk', 'name', 'salt_name', 'price', 'is_available', 'platform', 'last_updated']
+    search_fields = ['name', 'salt_name']
+    list_filter = ['platform', 'is_available']
 
 
 class MedicineCartView(admin.ModelAdmin):
