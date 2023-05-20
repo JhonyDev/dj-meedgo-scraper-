28806293cab535891f5449d91778f4d05340d2bb
jhonydev <star.jk901@gmail.com>
2023-05-20 16:48:16 +0500

    changes made

--- core/tests.py
+++ core/tests.py
@@ -1,79 +1,33 @@
-# import requests
-# from bs4 import BeautifulSoup
-#
-# # response = requests.get("https://pharmeasy.in/online-medicine-order/petril-md-0-25mg-tablet-19806")
-# # response = requests.get("https://pharmeasy.in/online-medicine-order/somo-strip-of-15-tablets-2987708")
-# response = requests.get("https://pharmeasy.in/online-medicine-order/sumo-strip-of-15-tablets-2709")
-# soup = BeautifulSoup(response.text, 'html.parser')
-# firsts = soup.find_all('td', {'class': 'DescriptionTable_field__l5jJ3'})
-# seconds = soup.find_all('td', {'class': 'DescriptionTable_value__0GUMC'})
-# generic_name = 0
-# price = None
-# name = soup.find('h1', {'class': 'MedicineOverviewSection_medicineName__dHDQi'}).text.strip()
-# try:
-#     price = soup.find('div', {'class': 'PriceInfo_ourPrice__jFYXr'}).text.strip()
-# except:
-#     try:
-#         price = soup.find('span', {'class': 'PriceInfo_striked__Hk2U_'}).text.strip()
-#     except:
-#         pass
-# if price:
-#     price = price.replace('MRP', '')
-#     price = price.replace('*', '')
-#     price = price.replace('₹', '')
-#     price = float(price)
-#
-# disc_price = None
-# try:
-#     disc_price = soup.find('div', class_='PriceInfo_gcdDiscountContainer__hr0YD').find('span').text
-#     if disc_price:
-#         disc_price = disc_price.replace('MRP', '')
-#         disc_price = disc_price.replace('*', '')
-#         disc_price = disc_price.replace('₹', '')
-#         disc_price = float(disc_price)
-# except:
-#     pass
-#
-# for first in firsts:
-#     if first.text.strip() == 'Contains':
-#         break
-#     generic_name += 1
-# is_available = True if price or disc_price else False
-# salt_name = seconds[generic_name].text.strip()
-#
-# print(name)
-# print(price or disc_price)
-# print(disc_price or price)
-# print(salt_name)
-# print(is_available)
+import asyncio
+from time import sleep
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from pyppeteer import launch
 
-options = Options()
-# options.add_argument('--headless')
-# options.add_argument("--force-device-scale-factor=0.5")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.1mg.com/drugs/famomeg-40mg-tablet-648333")
-name = driver.find_element(By.CLASS_NAME, "DrugHeader__title-content___2ZaPo").text
-salt_name = driver.find_element(By.CLASS_NAME, "DrugHeader__meta-value___vqYM0").text
-print(name)
-print(salt_name)
-try:
-    original_price = driver.find_element(By.CLASS_NAME, "DrugPriceBox__slashed-price___2UGqd").text.replace(
-        '₹', '')
-except:
-    original_price = None
-try:
-    discounted_price = driver.find_element(By.CLASS_NAME,
-                                           "DrugPriceBox__price___dj2lv").text.replace(
-        '₹', '')
-except:
-    discounted_price = None
-is_available = True if original_price or discounted_price else False
-print(name)
-print(salt_name)
-print(original_price or discounted_price)
-print(discounted_price or original_price)
-print(is_available)
+
+async def scrape_website():
+    # Launch the browser
+    browser = await launch()
+
+    # Create a new browser page
+    page = await browser.newPage()
+
+    # Navigate to the webpage
+    await page.setJavaScriptEnabled(True)
+    await page.goto('https://healthplus.flipkart.com/')
+    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
+
+    # Extract data from the webpage here
+    # You can use various methods provided by Pyppeteer to locate elements
+
+    # Example: Extracting text from an element with a specific selector
+    print(page)
+    print(page.url)
+    html = await page.content()
+    print(html)
+    sleep(100)
+    # Close the browser
+    await browser.close()
+
+
+# Run the scraping function
+asyncio.get_event_loop().run_until_complete(scrape_website())
