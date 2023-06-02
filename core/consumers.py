8d10d8a412266ac31fe0c8df4ceefe434b61bc39
jhonydev <star.jk901@gmail.com>
2023-06-02 05:16:22 +0500

    10 messages at a time

--- core/consumers.py
+++ core/consumers.py
@@ -50,9 +50,8 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
 
 def send_message_to_group(group_name, message):
-    from asgiref.sync import async_to_sync
     channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
+    channel_layer.group_send(
         group_name,
         {
             'type': 'send_message',
