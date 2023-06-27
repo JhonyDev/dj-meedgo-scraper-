08d02f193e6ca40325816d4b8df12d456092668d
jhonydev <star.jk901@gmail.com>
2023-06-27 22:02:56 +0500

    privacies added

--- src/website/views.py
+++ src/website/views.py
@@ -1,6 +1,14 @@
 from django.views.generic import TemplateView
 
 
+class ReturnsRefundsView(TemplateView):
+    template_name = "website/refunds_returns.html"
+
+
+class AboutUsView(TemplateView):
+    template_name = "website/about_us.html"
+
+
 class PrivacyPolicyView(TemplateView):
     template_name = "website/privacy_policy.html"
 
