b031a05100646556902df753099579b47ee88c1f
jhonydev <star.jk901@gmail.com>
2023-07-09 14:55:09 +0500

    search api enhanced

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,5 +1,4 @@
-from elasticsearch_dsl import Search
-from elasticsearch_dsl.query import MultiMatch
+from django.db.models import Case, When, Value
 
 from src.api.models import MedicineCartBridge, UserRating
 
@@ -31,63 +30,63 @@ def balance_medicines(instance):
 
 def get_similarity_queryset(queryset, param1, salt_name=None, is_salt=False):
     print("/" * 100)
-    # from fuzzywuzzy import fuzz
-    #
-    # similar_words = queryset.order_by(
-    #     'name', 'salt_name').distinct('name', 'salt_name').values('pk', 'name', 'salt_name')
-    # print(len(similar_words))
-    # similar_words_ = []
-    # similarities = []
-    # similarities_map = {}
-    # for word in similar_words:
-    #     ratio_name = fuzz.ratio(param1, word['name'])
-    #     if is_salt:
-    #         ratio_salt = fuzz.ratio(salt_name, word['salt_name'])
-    #         net_ratio = (ratio_salt + ratio_name) / 2
-    #     else:
-    #         ratio_salt = fuzz.ratio(param1, word['salt_name'])
-    #         net_ratio = (ratio_salt + ratio_name) / 2
-    #     check = 50 if not is_salt else 30
-    #     if net_ratio > check:
-    #         similar_words_.append(word['pk'])
-    #         similarities.append(net_ratio)
-    #         similarities_map[net_ratio] = word['pk']
-    #     if len(similarities) > 10:
-    #         break
-    # similarities.sort()
-    # similarities.reverse()
-    # queryset = queryset.filter(pk__in=similar_words_)
-    #
-    # sorted_similar_words = [similarities_map[x] for x in similarities]
-    # order_dict = {word: index for index, word in enumerate(sorted_similar_words)}
-    # queryset = queryset.annotate(custom_order=models.Case(
-    #     *[models.When(pk=pk, then=models.Value(order)) for pk, order in order_dict.items()],
-    #     default=models.Value(len(order_dict))
-    # ))
+    from fuzzywuzzy import fuzz
 
-    search = Search(index='medicine')
-    search = search.query(
-        MultiMatch(
-            query=param1,
-            fields=['name'],
-            fuzziness='AUTO',
-            prefix_length=2,
-            max_expansions=100,
-            tie_breaker=1.0
-        )
-    ).query(
-        MultiMatch(
-            query=salt_name,
-            fields=['salt_name'],
-            fuzziness='AUTO',
-            prefix_length=2,
-            max_expansions=100,
-            tie_breaker=1.0
-        )
-    )
-    results = search.execute()
-    results = [x.id for x in results.hits[:20]]
-    queryset = queryset.filter(pk__in=results)
+    similar_words = queryset.order_by(
+        'name', 'salt_name').distinct('name', 'salt_name').values('pk', 'name', 'salt_name')
+    print(len(similar_words))
+    similar_words_ = []
+    similarities = []
+    similarities_map = {}
+    for word in similar_words:
+        ratio_name = fuzz.ratio(param1, word['name'])
+        if is_salt:
+            ratio_salt = fuzz.ratio(salt_name, word['salt_name'])
+            net_ratio = (ratio_salt + ratio_name) / 2
+        else:
+            ratio_salt = fuzz.ratio(param1, word['salt_name'])
+            net_ratio = (ratio_salt + ratio_name) / 2
+        check = 50 if not is_salt else 30
+        if net_ratio > check:
+            similar_words_.append(word['pk'])
+            similarities.append(net_ratio)
+            similarities_map[net_ratio] = word['pk']
+        if len(similarities) > 10:
+            break
+    similarities.sort()
+    similarities.reverse()
+    queryset = queryset.filter(pk__in=similar_words_)
+
+    sorted_similar_words = [similarities_map[x] for x in similarities]
+    order_dict = {word: index for index, word in enumerate(sorted_similar_words)}
+    queryset = queryset.annotate(custom_order=Case(
+        *[When(pk=pk, then=Value(order)) for pk, order in order_dict.items()],
+        default=Value(len(order_dict))
+    ))
+
+    # search = Search(index='medicine')
+    # search = search.query(
+    #     MultiMatch(
+    #         query=param1,
+    #         fields=['name'],
+    #         fuzziness='AUTO',
+    #         prefix_length=2,
+    #         max_expansions=100,
+    #         tie_breaker=1.0
+    #     )
+    # ).query(
+    #     MultiMatch(
+    #         query=salt_name,
+    #         fields=['salt_name'],
+    #         fuzziness='AUTO',
+    #         prefix_length=2,
+    #         max_expansions=100,
+    #         tie_breaker=1.0
+    #     )
+    # )
+    # results = search.execute()
+    # results = [x.id for x in results.hits[:20]]
+    # queryset = queryset.filter(pk__in=results)
 
     return queryset
 
